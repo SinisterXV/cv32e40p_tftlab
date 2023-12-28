b8912e8b40fd761cc494f41387442647034f6c18
SinisterXV <jg.silvestri@yahoo.com>
2023-12-28 10:27:53 +0100

    Implemented fsim campaign script

---
+++ run_fsim_campaign.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+function perform_fsim_iteration {
+	make zoix/fgen/sdd K=${k} # generate fault list
+	SECONDS=0 # reset timer
+	make zoix/fsim-timing FAULT_LIST=run/zoix_timing/cv32e40p_top_sdd_K${k}.rpt # run fault sim
+
+	# read line 6147 from the report: it contains the fault coverage
+	# then isolate the two percentages
+	# then select only the first (they are identical)
+	fault_coverage=$(sed -n '6142p' run/zoix_timing/cv32e40p_top_sdd_K${k}.rpt.fsim | grep -Eo "[0-9.]+%" | head -1)
+	{
+		echo "K = ${k}"
+		echo "Simulation duration: ${SECONDS}s"
+		echo "Fault coverage: $fault_coverage"
+		echo ""
+	} >> $rpt_file
+}
+
+echo "Starting fault simulation campaign"
+
+rpt_file="reports/fsim_campaign.rpt"
+true > $rpt_file # clear report
+{
+	echo "FAULT CAMPAIGN REPORT"
+	echo "Started on $(date)" 
+	echo ""
+} >> $rpt_file
+
+# iterate over the interval [1; 2] with a step of 0.1
+for k in $(seq 1 0.1 2); do
+	perform_fsim_iteration
+done
+
+# iterate over the interval [3; 10] with a step of 1
+for k in $(seq 3 10); do
+	perform_fsim_iteration
+done
+
+# iterate over the interval [20; 90] with a step of 10
+for k in $(seq 20 10 90); do
+	perform_fsim_iteration
+done
+
+# use the maximum k value, found experimentally
+k=94
+perform_fsim_iteration
+
+echo "Finished execution on $(date)" >> $rpt_file
+
+# save all report to the `reports` folder so they survive a `make clean`
+mv run/zoix_timing/cv32e40p_top_sdd_K*.rpt.fsim reports/
